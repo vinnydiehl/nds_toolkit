ef8f53b3df02d1c64c2156db2328d7007ee33ec5
Vinny Diehl <gbchaosmaster926@gmail.com>
2012-06-17 21:31:49 -0400

    Added Clipboard class

---
+++ src/clipboard/Clipboard.cpp
@@ -0,0 +1,48 @@
+/**
+ * NDS Hacking Kit - A suite of tools for hacking Nintendo video games.
+ * Copyright (C) 2012 Vinny Diehl
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+**/
+
+/**
+ * @file Clipboard.cpp - Class with static methods for clipboard handling.
+**/
+
+#include "Clipboard.h"
+
+void Clipboard::SetClipboard(wxString str)
+{
+    if (wxTheClipboard->Open())
+    {
+        wxTheClipboard->SetData(new wxTextDataObject(str));
+        wxTheClipboard->Close();
+    }
+}
+wxString Clipboard::GetClipboard(void)
+{
+    wxString str = _T("");
+    if (wxTheClipboard->Open())
+    {
+        if (wxTheClipboard->IsSupported(wxDF_TEXT))
+        {
+            wxTextDataObject data;
+            wxTheClipboard->GetData(data);
+            str = data.GetText();
+        }
+        wxTheClipboard->Close();
+    }
+    return str;
+}
+
---
+++ src/clipboard/Clipboard.h
@@ -0,0 +1,37 @@
+/**
+ * NDS Hacking Kit - A suite of tools for hacking Nintendo video games.
+ * Copyright (C) 2012 Vinny Diehl
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+**/
+
+/**
+ * @file Clipboard.h - Header file for the Clipboard class.
+**/
+
+#ifndef CLIPBOARD_H_INCLUDED
+#define CLIPBOARD_H_INLCUDED
+
+#include <wx/clipbrd.h>
+#include <wx/string.h>
+
+class Clipboard
+{
+public:
+    static void SetClipboard(wxString str);
+    static wxString GetClipboard(void);
+};
+
+#endif // CLIPBOARD_H_INLCUDED
+
