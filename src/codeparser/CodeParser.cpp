9eeb1dd6a8094cc49a7da213c61bca82ea5ae424
Vinny Diehl <gbchaosmaster926@gmail.com>
2012-06-22 12:17:15 -0400

    Added definitions for CodeParser class
    The class and Makefile are in place in the codeparser directory. A few
    methods that I think will be needed have been defined and currently have
    placeholder return values to stop warnings when I was testing the build.
    This is not linked at all anywhere else in the code, including the main
    build system! Don't do that until it is implemented and ready to be
    used.

---
+++ src/codeparser/CodeParser.cpp
@@ -0,0 +1,45 @@
+/**
+ * NDS Hacking Kit - A suite of tools for hacking Nintendo video games.
+ * Copyright (C) 2012 Vinny Diehl
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+**/
+
+/**
+ * @file CodeParser.cpp - Class with static methods for code verification.
+ *
+ * See #1 on the issue tracker.
+**/
+
+#include "CodeParser.h"
+
+/** Public Methods **/
+
+wxString CodeParser::Beautify(wxString code)
+{
+    return _T("");
+}
+
+bool CodeParser::Verify(wxString code)
+{
+    return true;
+}
+
+/** Private Methods **/
+
+wxString CodeParser::mStripChar(wxString str, wxChar ch)
+{
+    return _T("");
+}
+
---
+++ src/codeparser/CodeParser.h
@@ -0,0 +1,41 @@
+/**
+ * NDS Hacking Kit - A suite of tools for hacking Nintendo video games.
+ * Copyright (C) 2012 Vinny Diehl
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+**/
+
+/**
+ * @file CodeParser.h - Header file for the CodeParser class.
+**/
+
+#pragma once
+
+#ifndef CODEPARSER_H_INCLUDED
+#define CODEPARSER_H_INLCUDED
+
+#include <wx/string.h>
+
+class CodeParser
+{
+public:
+    static wxString Beautify(wxString code);
+    static bool Verify(wxString code);
+
+private:
+    wxString mStripChar(wxString str, wxChar ch=_T(' '));
+};
+
+#endif // CODEPARSER_H_INLCUDED
+
